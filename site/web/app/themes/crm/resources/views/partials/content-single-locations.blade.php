84066a038ca49eb047644bbafa25c232df55fb64
Joshua Busnelli <info@minemindmedia.com>
2022-06-14 15:35:35 -0500

    stuff

--- site/web/app/themes/crm/resources/views/partials/content-single-locations.blade.php
+++ site/web/app/themes/crm/resources/views/partials/content-single-locations.blade.php
@@ -2,21 +2,47 @@
 
 <div class="entry-content">
 	
-						<?php 
+	<div class="entry-content">
 
-						$locations = get_sub_field('locations');
+		<?php while ( have_posts() ) : the_post(); ?>
 
-						?>
-						<?php if( $locations ): ?>
-							<ul>
-							<?php foreach( $locations as $location ): ?>
-								<li>
-									<a href="<?php echo get_permalink( $location->ID ); ?>">
-										<?php echo get_the_title( $location->ID ); ?>
-									</a>
-								</li>
-							<?php endforeach; ?>
-							</ul>
-						<?php endif; ?>
+		<?php
 
-					</div>
+		/*
+		*  Query posts for a relationship value.
+		*  This method uses the meta_query LIKE to match the string "123" to the database value a:1:{i:0;s:3:"123";} (serialized array)
+		*/
+
+		$staff = get_posts(array(
+			'post_type' => 'contacts',
+			'meta_query' => array(
+				array(
+					'key' => 'location', // name of custom field
+					'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+					'compare' => 'LIKE'
+				)
+			)
+		));
+
+		?>
+		<div class="people-grid">
+		<?php if( $staff ): ?>
+			<?php foreach( $staff as $doctor ): ?>
+				<?php
+
+				$photo = get_field('photo', $doctor->ID);
+
+				?>
+				<div class="single-person">
+						<img src="<?php echo $photo['url']; ?>" alt="<?php echo $photo['alt']; ?>" />
+						<h3><?php echo get_the_title( $doctor->ID ); ?></h3>
+						<h4><?php echo get_field('position_title', $doctor->ID); ?></h3>
+
+				</div>
+			<?php endforeach; ?>
+		</div>
+		<?php endif; ?>
+
+<?php endwhile; // end of the loop. ?>'''
+	 
+	 </div>
