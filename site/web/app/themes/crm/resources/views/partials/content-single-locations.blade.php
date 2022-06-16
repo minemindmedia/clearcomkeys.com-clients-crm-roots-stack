728d38d29072b978b87fa40267a25ebb48547e4b
ahmadnawaz121 <ahmadnawaz121@gmail.com>
2022-06-16 21:46:55 +0500

    display contact list to location page

--- site/web/app/themes/crm/resources/views/partials/content-single-locations.blade.php
+++ site/web/app/themes/crm/resources/views/partials/content-single-locations.blade.php
@@ -5,6 +5,7 @@
 						<?php 
 						global $wpdb;
 						$locations = get_field('location');
+						$locationName = $locations;
 						$companies = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_type='companies' AND post_status='publish'");
 						$locationArray = [];
 						if($companies) {
@@ -50,3 +51,50 @@
 						</div>
 					</div>
 				<?php endif; ?>
+				<div class="flex-1 mt-4 overflow-hidden bg-white shadow sm:rounded-lg">
+					<div class="px-4 py-5 sm:px-6">
+						<h3 class="text-lg font-medium leading-6 text-gray-900">Contact list associated with this location</h3>
+						<p class="max-w-2xl mt-1 text-sm text-gray-500">
+						</p>
+					</div>
+					<div class="border-t border-gray-200">
+					<?php
+					$contacts = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_type='contacts' AND post_status='publish'");
+					if($contacts) {
+						foreach ($contacts as $contact) {
+							$location = get_field('location_relationship', $contact->ID);
+							if(isset($location['assign_location']) && $location['assign_location']) {
+								if(!empty($location) && isset($location['location'][0]->post_title) && $location['location'][0]->post_title == $locationName) {
+									?>
+									@if( have_rows('contact_details', $contact->ID) )
+										@while( have_rows('contact_details', $contact->ID) )
+										@php
+											the_row();
+											$position_title = get_sub_field('position_title', $contact->ID);
+										@endphp
+				
+										@if( have_rows('phone_numbers', $contact->ID) )
+											@while( have_rows('phone_numbers', $contact->ID) )
+				
+											@php
+												the_row();
+												$phone = get_sub_field('phone', $contact->ID);
+												$type = get_sub_field('phone_type', $contact->ID);
+											@endphp
+											@if($position_title != 'Key Tech')
+												<div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+													<dt class="text-sm font-medium text-gray-500">{{ $type }}: {{ $phone->national }}</dt>
+												</div>
+											@endif
+											@endwhile
+										@endif
+										@endwhile
+									@endif
+									<?php
+								}
+							}
+						}
+					}
+					?>
+					</div>
+				</div>
