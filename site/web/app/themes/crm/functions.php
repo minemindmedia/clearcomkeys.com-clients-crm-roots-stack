6f8d4f6c1fe8712f26012f5c6b6abfec0f4c2555
Joshua Busnelli <info@minemindmedia.com>
2022-07-01 11:46:28 -0500

    lockdown site

--- site/web/app/themes/crm/functions.php
+++ site/web/app/themes/crm/functions.php
@@ -233,3 +233,41 @@ function my_acfe_save_category($post_id, $object){
     get_field('location');
 
 }
+
+/**
+ * Change wp-admin to home page
+ */
+add_filter('login_url', 'change_login_url');
+function change_login_url() {
+    return home_url('/');
+}
+
+/**
+ * Redirect wp-login.php to home page
+ */
+add_action('init','custom_login');
+
+function custom_login(){
+ global $pagenow;
+ if( 'wp-login.php' == $pagenow && (isset($_GET['action']) ? $_GET['action'] : '') !="logout") {
+  wp_redirect('/');
+  exit();
+ }
+}
+
+/**
+ * Logout - Avoid Confirmation
+ */
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : '/';
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
--- site/web/app/themes/crm/resources/views/layouts/app.blade.php
+++ site/web/app/themes/crm/resources/views/layouts/app.blade.php
@@ -1,21 +1,27 @@
-<a class="sr-only focus:not-sr-only" href="#main">
-  {{ __('Skip to content') }}
-</a>
-
-@include('sections.header')
-
-<div class="min-h-full">
-  <div class="pb-32 bg-indigo-600">
-    @include('partials.page-header')
+@if( ! is_user_logged_in() )
+  <div class="w-[400px] mx-auto mt-[200px] block">
+    {!! do_shortcode('[lwa]') !!}
   </div>
-  <main class="-mt-32">
-    <div class="px-4 pb-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="px-5 py-6 bg-gray-100 rounded-lg shadow sm:px-6">
-          @yield('content')
-      </div>
+@else 
+  <a class="sr-only focus:not-sr-only" href="#main">
+    {{ __('Skip to content') }}
+  </a>
+
+  @include('sections.header')
+
+  <div class="min-h-full">
+    <div class="pb-32 bg-indigo-600">
+      @include('partials.page-header')
     </div>
-  </main>
-</div>
+    <main class="-mt-32">
+      <div class="px-4 pb-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="px-5 py-6 bg-gray-100 rounded-lg shadow sm:px-6">
+            @yield('content')
+        </div>
+      </div>
+    </main>
+  </div>
 
 
-@include('sections.footer')
+  @include('sections.footer')
+@endif
