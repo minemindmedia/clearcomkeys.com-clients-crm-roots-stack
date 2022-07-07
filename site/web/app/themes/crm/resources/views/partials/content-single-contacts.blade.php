93706552d4995a791bb0885ca63c294c0c5cf83b
Joshua Busnelli <info@minemindmedia.com>
2022-07-07 12:12:27 -0500

    debug training date

--- site/web/app/themes/crm/resources/views/partials/content-single-contacts.blade.php
+++ site/web/app/themes/crm/resources/views/partials/content-single-contacts.blade.php
@@ -105,9 +105,6 @@
                   <dt class="text-sm font-medium text-gray-500">Title</dt>
                   <dd class="mt-1 text-sm text-gray-900">
                     @if( $contact['position_title'] )
-                      @if( $contact['position_title'] == 'Key Tech')
-                        <span>&#42;</span>
-                      @endif 
                       {{ $contact['position_title'] }}
                     @endif
                   </dd>
@@ -197,13 +194,7 @@
       <section aria-labelledby="timeline-title" class="lg:col-start-3 lg:col-span-1">
         <div class="px-4 py-5 bg-white shadow sm:rounded-lg sm:px-6">
           <h2 id="timeline-title" class="text-lg font-medium text-gray-900">Training Dates</h2>
-					@if( have_rows('training') )
-          @while( have_rows('training') )
-                      
-                      @php
-                        the_row();
-                
-                      @endphp
+							
             @if( have_rows('training_dates') )
               <div class="flow-root mt-6">
                 <ul role="list" class="-mb-8">
@@ -244,8 +235,7 @@
                 </ul>
               </div>
             @endif
-            @endwhile
-          @endif
+      
         </div>
       </section>
 
