dc9f5b5e19531ec766b6b94256e0a9266875ac62
Joshua Busnelli <info@minemindmedia.com>
2022-07-07 12:08:07 -0500

    debug phone

--- site/web/app/themes/crm/resources/views/partials/content-single-companies.blade.php
+++ site/web/app/themes/crm/resources/views/partials/content-single-companies.blade.php
@@ -64,7 +64,7 @@
       </div>
       <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">Phone</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $s_phone->national }}</dd>
+        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $s_phone }}</dd>
       </div>
       <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">Fax</dt>
@@ -95,7 +95,7 @@
       </div>
       <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">Phone</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $b_phone->national }}</dd>
+        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $b_phone }}</dd>
       </div>
       <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">Fax</dt>
